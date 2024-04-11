88f9f8510e379ed77a487e424105f7ca3dffad6b
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-04-12 05:59:27 +0700

    Update convert.py

--- data/convert.py
+++ data/convert.py
@@ -128,7 +128,10 @@ def parse_list_file(link, output_directory):
     with open(file_name, 'w', encoding='utf-8') as output_file:
         json.dump(sort_dict(result_rules), output_file, ensure_ascii=False, indent=2)
 
-
+    srs_path = file_name.replace(".json", ".srs")
+    os.system(f"sing-box rule-set compile --output {srs_path} {file_name}")
+    return file_name
+    
 with open("./data/source.txt", 'r') as links_file:
     links = links_file.read().splitlines()
 
