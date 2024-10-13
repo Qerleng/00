d39bab4164234525049f6f933f99461aaf0fcf74
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-13 19:13:37 +0700

    Update update.sh

--- data/update.sh
+++ data/update.sh
@@ -1,6 +1,4 @@
-wget https://github.com/DustinWin/clash_singbox-tools/releases/download/mihomo/mihomo-alpha-linux-amd64.tar.gz -O - | tar -zxf - -C ./tools/
-mv -f ./tools/CrashCore ./tools/mihomo
-wget https://github.com/DustinWin/clash_singbox-tools/releases/download/sing-box/sing-box-release-linux-amd64v3.tar.gz -O - | tar -zxf - -C ./tools/
-mv -f ./tools/CrashCore ./tools/sing-box
+wget https://github.com/DustinWin/clash_singbox-tools/releases/download/sing-box/sing-box-release-linux-amd64v3.tar.gz -O - | tar -zxf - -C ./sing-box/
+mv -f ./sing-box/CrashCore ./sing-box/sing-box
 
 
