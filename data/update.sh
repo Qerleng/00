fda24f51001c8e2f4cab96dca751e9766b283be9
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-16 13:21:06 +0700

    Delete data/update.sh

--- data/update.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/DustinWin/clash_singbox-tools/releases/download/sing-box/sing-box-release-linux-amd64v3.tar.gz -O - | tar -zxf - -C ./sing-box/
-mv -f ./sing-box/CrashCore ./sing-box/sing-box
-
-
